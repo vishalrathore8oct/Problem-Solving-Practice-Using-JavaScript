eaa9fee1f5258e4779d6c2c8ec28d07e5259ba74
Vishal <vishal.rathore8oct@gmail.com>
2026-07-01 07:13:16 +0530

    Add input utility function for user prompts using built-in readline module

---
+++ utils/input.utils.js
@@ -0,0 +1,17 @@
+import { createInterface } from "readline";
+
+function input(promptText = "") {
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise((resolve) => {
+    rl.question(promptText, (answer) => {
+      rl.close();
+      resolve(answer.trim());
+    });
+  });
+}
+
+export { input };
